39a7b1596697070ccf1e3f225e2713d0a41080a4
patfitz95 <patfitz95@gmail.com>
2020-12-11 19:50:36 -0600

    trying to deploy

---
+++ build/precache-manifest.8945e48e081229b0bbdfcbe36c1ede80.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "437f4b09375358a36ea20c0a9c6abd8e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "950906c162907bd1d2ab",
+    "url": "/static/css/main.ba194a07.chunk.css"
+  },
+  {
+    "revision": "b970eb330974e9404de6",
+    "url": "/static/js/2.15e0e4ca.chunk.js"
+  },
+  {
+    "revision": "ef90dec32da5a94b550496958cb808df",
+    "url": "/static/js/2.15e0e4ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "950906c162907bd1d2ab",
+    "url": "/static/js/main.a3bc65d9.chunk.js"
+  },
+  {
+    "revision": "c572ac9c8263f317d1e5",
+    "url": "/static/js/runtime-main.a32808dc.js"
+  }
+]);
